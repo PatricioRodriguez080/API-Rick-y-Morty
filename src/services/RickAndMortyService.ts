0ffcdaba5bf857eaec5af383ea8e495181c532e7
Patricio Rodriguez <patriciorodriguez@Mac-Pato.local>
2025-03-09 20:30:22 -0300

    Cambio en getCharacters para poder obtener mas resultados de personajes

--- src/services/RickAndMortyService.ts
+++ src/services/RickAndMortyService.ts
@@ -2,9 +2,15 @@ import { Character } from "../types/RickAndMortyTypes"
 
 export const getCharacters = async (): Promise<Character[] | null> => {
     try {
-        const response = await fetch("https://rickandmortyapi.com/api/character")
-        const data = await response.json()
-        return data.results
+        const urls = [
+            "https://rickandmortyapi.com/api/character?page=1",
+            "https://rickandmortyapi.com/api/character?page=2"
+        ]
+
+        const responses = await Promise.all(urls.map(url => fetch(url)))
+        const data = await Promise.all(responses.map(res => res.json()))
+
+        return data.flatMap(d => d.results)
     } catch (error) {
         console.log(error)
         return null
