da4080da20a0fa46e0e3b1fdfffb9e72a73ee1ba
Patricio Rodriguez <patriciorodriguez@Mac-Pato.local>
2025-03-11 16:05:40 -0300

    Creacion de navegacion desde personajes favoritos hacia los detalles

--- Components/FavoriteCharacters/FavoriteCharacterCard.tsx
+++ Components/FavoriteCharacters/FavoriteCharacterCard.tsx
@@ -1,4 +1,5 @@
 import { Character } from "@/src/types/RickAndMortyTypes"
+import Link from "next/link"
 
 interface FavoriteCharacterCardProps {
     character: Character
@@ -11,7 +12,9 @@ const FavoriteCharacterCard = ({ character }: FavoriteCharacterCardProps) => {
                 <img src={character.image} className="w-48 h-full object-cover rounded-l-lg" alt={character.name} />
             </div>
             <div className="flex flex-col justify-center ml-4 p-4">
-                <h1 className="text-xl font-semibold">{character.name}</h1>
+                <Link href={`/characters/${character.id}`}>
+                    <h1 className="text-xl font-semibold text-white hover:text-gray-300 transform hover:scale-105 transition-all duration-200">{character.name}</h1>
+                </Link>
                 <div className="flex items-center mt-2">
                     <div className={`rounded-full w-2 h-2 ${character.status === "Alive" ? "bg-green-500" : "bg-red-500"}`}></div>
                     <h3 className="ml-2">{character.status}</h3>
